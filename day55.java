4a7f1e17758522fca976c6768d5cb0cffe915030
amannayak2003 <amannayak774@gmail.com>
2023-11-18 09:26:42 +0530

    Reverse a double linkedlist

---
+++ day55.java
@@ -0,0 +1,17 @@
+public class day55 {
+    public static Node reverseDLL(Node  head)
+{
+    if(head.next == null) {
+        return head;
+    }
+    Node current = head;
+    Node temp = null;
+    while(current != null) {
+        temp = current.prev;
+        current.prev = current.next;
+        current.next = temp;
+        current = current.prev;
+    }
+    return temp.prev;
+}
+}
